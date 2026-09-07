8a4e2e486a9f0324c448c0b2a35ed1c0908d6e20
jidonusegun <jidonu2@gmail.com>
2026-09-07 18:24:56 +0100

    added SSL rejectUnauthorized to false on db.js

--- src/db.js
+++ src/db.js
@@ -2,12 +2,4 @@ import pg from 'pg';
 import { env } from './config/env.js';
 const url = new URL(env.databaseUrl);
 
-console.log({
-  protocol: url.protocol,
-  username: url.username,
-  passwordExists: !!url.password,
-  hostname: url.hostname,
-  database: url.pathname,
-});
-
-export const pool = new pg.Pool({ connectionString: env.databaseUrl });
+export const pool = new pg.Pool({ connectionString: env.databaseUrl, ssl: { rejectUnauthorized: false } });
